fbe91b328470600078f63d2abedb2ebee15a0ba3
CAFE2l <pciskolargx@gmail.com>
2026-03-12 14:48:41 -0300

    class 8 finished

--- Aulas/Aula01/index.js
+++ Aulas/Aula01/index.js
@@ -13,7 +13,32 @@ const cores = {
 const fundo = {
     branco: "\x1b[40m",
     vermelho: "\x1b[41m",
-    verde: "\x1b[42m",
+    cinza_claro: "\x1b[107m"
+};
+
+const estilos = {
+    reset: "\x1b[0m",
+    negrito: "\x1b[1m",
+    fraco: "\x1b[2m",
+    italico: "\x1b[3m",
+    sublinhado: "\x1b[4m",
+    inverso: "\x1b[7m",
+    invisivel: "\x1b[8m",
+    tachado: "\x1b[9m"
+};
+
+// Variáveis de apoio
+const terreno = "Procurando Alunos";
+const separadorSuperior = "===".repeat(5);
+const separadorInferior = "===".repeat(12) + "=";
+
+// Lógica de centralização manual (o JS não tem o .center do Python nativo)
+const centralizado = terreno.padStart((40 + terreno.length) / 2).padEnd(40);
+
+// Execução do Print
+console.log(`${estilos.negrito}${cores.vermelho}${separadorSuperior}${cores.cinza}Alunos${cores.azul}${separadorSuperior}${cores.limpa}`);
+console.log(`${cores.cinza}${estilos.negrito}${centralizado}${cores.limpa}`);
+console.log(`${estilos.negrito}${cores.verde}${separadorInferior}${cores.limpa}`);    verde: "\x1b[42m",
     amarelo: "\x1b[43m",
     azul: "\x1b[44m",
     roxo: "\x1b[45m",
@@ -60,10 +85,13 @@ const listaDeAlunos = [alunos, medias];
 
 function exibir(aluno){
   if(listaDeAlunos[0].includes(aluno)){
+    // const alunos = listaDeAlunos[0];
+    // const medias = listaDeAlunos[1];
+    
+    const [alunos, medias] = listaDeAlunos
 
-
-    const indice = listaDeAlunos[0].indexOf(aluno);
-    const mediaAluno = listaDeAlunos[1][indice];
+    const indice = alunos.indexOf(aluno);
+    const mediaAluno = medias[indice];
     console.log(indice);
     console.log(`${cores.cinza}${estilos.negrito}O aluno ${cores.verde}${aluno} ${cores.cinza}esta cadastrado! e está com a nota ${cores.verde}${mediaAluno}${cores.limpa}`);
   }

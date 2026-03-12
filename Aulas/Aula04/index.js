9967b974d0c1d2d6dd9e937ffacabd300591b581
CAFE2l <pciskolargx@gmail.com>
2026-03-12 15:16:43 -0300

    Class number 10 finished

---
+++ Aulas/Aula04/index.js
@@ -0,0 +1,57 @@
+const cores = {
+    limpa: "\x1b[0m",
+    vermelho: "\x1b[31m",
+    verde: "\x1b[32m",
+    amarelo: "\x1b[33m",
+    azul: "\x1b[34m",
+    roxo: "\x1b[35m",
+    ciano: "\x1b[36m",
+    cinza: "\x1b[37m",
+    pretoebranco: "\x1b[7;30m"
+};
+
+const fundo = {
+    branco: "\x1b[40m",
+    vermelho: "\x1b[41m",
+    cinza_claro: "\x1b[107m"
+};
+
+const estilos = {
+    reset: "\x1b[0m",
+    negrito: "\x1b[1m",
+    fraco: "\x1b[2m",
+    italico: "\x1b[3m",
+    sublinhado: "\x1b[4m",
+    inverso: "\x1b[7m",
+    invisivel: "\x1b[8m",
+    tachado: "\x1b[9m"
+};
+
+// Variáveis de apoio
+const terreno = "Estrutura de um for of";
+const separadorSuperior = "===".repeat(5);
+const separadorInferior = "===".repeat(12);
+
+// Lógica de centralização manual (o JS não tem o .center do Python nativo)
+const centralizado = terreno.padStart((40 + terreno.length) / 2).padEnd(40);
+
+// Execução do Print
+console.log(`${estilos.negrito}${cores.vermelho}${separadorSuperior}${cores.cinza}ForOF${cores.azul}${separadorSuperior}${cores.limpa}`);
+console.log(`${cores.cinza}${estilos.negrito}${centralizado}${cores.limpa}`);
+console.log(`${estilos.negrito}${cores.verde}${separadorInferior}${cores.limpa}`);
+
+
+
+const notas = [10, 6.4, 8, 7.5];
+
+let somaNotas = 0;
+
+for(let nota of notas){
+  somaNotas += nota
+}
+
+const media = somaNotas / notas.length;
+
+console.log(`${cores.cinza}${estilos.negrito}A soma entre todas as notas é de ${estilos.italico}${cores.ciano}${somaNotas}${cores.limpa}`);
+
+console.log(`${cores.cinza}${estilos.negrito}A média entre as notas é de ${cores.ciano}${estilos.italico}${media}${cores.limpa}`)

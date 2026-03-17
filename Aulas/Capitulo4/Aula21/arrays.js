dd5b94085b6b93bdacfb3f179de3364ac5152e04
CAFE2l <pciskolargx@gmail.com>
2026-03-17 15:19:45 -0300

    starting chapter 4

---
+++ Aulas/Capitulo4/Aula21/arrays.js
@@ -0,0 +1,17 @@
+const edFolha = [
+  { titulo: "HTML e CSS na Prática", preco: 45.50 },
+  { titulo: "JavaScript Essencial", preco: 59.90 },
+  { titulo: "Node.js Avançado", preco: 72.00 },
+  { titulo: "Clean Architecture", preco: 84.50 },
+  { titulo: "Clean Code", preco: 89.90 }
+]
+
+const edGalho = [
+  { titulo: "Git e GitHub Completo", preco: 42.30 },
+  { titulo: "Python para Iniciantes", preco: 49.90 },
+  { titulo: "Estruturas de Dados em JS", preco: 55.25 },
+  { titulo: "Dominando TypeScript", preco: 64.00 },
+  { titulo: "The Clean Coder", preco: 79.90 }
+]
+
+module.exports = {edFolha, edGalho}

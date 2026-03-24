1b89226db89b860d3a49880a76d5776c7180e05a
CAFE2l <pciskolargx@gmail.com>
2026-03-24 16:25:48 -0300

    async and await

--- Aulas/Capitulo5/Aula01/index.js
+++ Aulas/Capitulo5/Aula01/index.js
@@ -49,24 +49,27 @@ function trataErro(erro){
   throw new Error(chalk.red(erro.code, "Nao há nenhum um arquivo no diretorio"));
 
 }
+//async & await
 
-function pegaArquivo(caminhoDoArquivo){
-  const encoding = 'utf-8';
-  fs.promises
-    .readFile(caminhoDoArquivo, encoding);
-    .then((texto) => console.log(chalk.green(texto));
-    .catch(trataErro);
+async function pegaArquivo(caminhoDoArquivo){
+  try{
+   const encoding = 'utf-8';
+   const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+  console.log(chalk.green(texto));
+  } catch(erro){
+  trataErro(erro);
+  }
 }
 
+// promises com then()
 // function pegaArquivo(caminhoDoArquivo){
-//   const encoding ='utf-8';
-//   fs.readFile(caminhoDoArquivo,encoding, (erro,texto) => {
-//     if(erro){
-//       trataErro(erro);
-//     }
-//     console.log(chalk.green(texto));
-//   })
+//   const encoding = 'utf-8';
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding);
+//     .then((texto) => console.log(chalk.green(texto));
+//     .catch(trataErro);
 // }
 
 
 pegaArquivo('./package.json');
+pegaArquivo('./package');

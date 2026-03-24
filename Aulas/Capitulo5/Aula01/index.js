f49bed45dde290a5ec4e42b7523e605de7f95b3f
CAFE2l <pciskolargx@gmail.com>
2026-03-24 16:09:59 -0300

    understanding methods

--- Aulas/Capitulo5/Aula01/index.js
+++ Aulas/Capitulo5/Aula01/index.js
@@ -1,1 +1,72 @@
-console.log("GAY");
+import fs from 'fs';
+import chalk from 'chalk';
+
+
+const cores = {
+    limpa: "\x1b[0m",
+    vermelho: "\x1b[31m",
+    verde: "\x1b[32m",
+    amarelo: "\x1b[33m",
+    azul: "\x1b[34m",
+    roxo: "\x1b[35m",
+    ciano: "\x1b[36m",
+    cinza: "\x1b[37m",
+    pretoebranco: "\x1b[7;30m"
+};
+
+const fundo = {
+    branco: "\x1b[40m",
+    vermelho: "\x1b[41m",
+    cinza_claro: "\x1b[107m"
+};
+
+const estilos = {
+    reset: "\x1b[0m",
+    negrito: "\x1b[1m",
+    fraco: "\x1b[2m",
+    italico: "\x1b[3m",
+    sublinhado: "\x1b[4m",
+    inverso: "\x1b[7m",
+    invisivel: "\x1b[8m",
+    tachado: "\x1b[9m"
+};
+
+// Variáveis de apoio
+const terreno = "nao estou entendendo nada";
+const separadorSuperior = "===".repeat(5);
+const separadorInferior = "===".repeat(12) + "=";
+
+// Lógica de centralização manual (o JS não tem o .center do Python nativo)
+const centralizado = terreno.padStart((40 + terreno.length) / 2).padEnd(40);
+
+// Execução do Print
+console.log(`${estilos.negrito}${cores.vermelho}${separadorSuperior}${cores.cinza}ComoAssim${cores.azul}${separadorSuperior}${cores.limpa}`);
+console.log(`${cores.cinza}${estilos.negrito}${centralizado}${cores.limpa}`);
+console.log(`${estilos.negrito}${cores.verde}${separadorInferior}${cores.limpa}`);
+
+function trataErro(erro){
+  console.log(erro)
+  throw new Error(chalk.red(erro.code, "Nao há nenhum um arquivo no diretorio"));
+
+}
+
+function pegaArquivo(caminhoDoArquivo){
+  const encoding = 'utf-8';
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding);
+    .then((texto) => console.log(chalk.green(texto));
+    .catch(trataErro);
+}
+
+// function pegaArquivo(caminhoDoArquivo){
+//   const encoding ='utf-8';
+//   fs.readFile(caminhoDoArquivo,encoding, (erro,texto) => {
+//     if(erro){
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   })
+// }
+
+
+pegaArquivo('./package.json');

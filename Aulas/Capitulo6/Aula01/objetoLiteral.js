3df6d47bac327eb0abb0a9f233098437255a9e23
CAFE2l <pciskolargx@gmail.com>
2026-03-26 19:07:06 -0300

    chapter 6 started

---
+++ Aulas/Capitulo6/Aula01/objetoLiteral.js
@@ -0,0 +1,24 @@
+const user = {
+  nome: "Joao",
+  email: "Joao@gmail.com",
+  nascimento: "2009/02/01",
+  role: "estudante",
+  ativo: true,
+  exibirInfos: function(){
+  console.log(this.nome, this.email);
+  }
+}
+ 
+const admin = {
+  nome: "cleide", 
+  email: "queDelicia@gmail.com",
+  role: "admin",
+  criarCurso(){
+    console.log('curso criado!')
+  }
+}
+
+
+Object.setPrototypeOf(admin, user)
+admin.criarCurso()
+admin.exibirInfos()
